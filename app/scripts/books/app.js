7e3f0dcebabcb4a628e7cb3621015a526bca781b
Jack Hsu <jackh@nulogy.com>
2013-09-19 23:22:16 -0400

    Upgrades angular and adds animation

--- app/scripts/books/app.js
+++ app/scripts/books/app.js
@@ -2,14 +2,18 @@
 
 define([
   'angular',
+  'angular-animate',
   'angular-cookies',
   'angular-resource',
+  'angular-route',
   'angular-fire'
 ], function(angular) {
 
   return angular.module('booksApp', [
+    'ngAnimate',
     'ngCookies',
     'ngResource',
+    'ngRoute',
     'firebase'
   ]);
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,10 @@ require({
     'jquery': '../bower_components/jquery/jquery',
     'lodash': '../bower_components/lodash/dist/lodash',
     'angular': '../bower_components/angular/angular',
+    'angular-animate': '../bower_components/angular-animate/angular-animate',
     'angular-cookies': '../bower_components/angular-cookies/angular-cookies',
     'angular-resource': '../bower_components/angular-resource/angular-resource',
+    'angular-route': '../bower_components/angular-route/angular-route',
     'angular-sanitize': '../bower_components/angular-sanitize/angular-sanitize',
     'angular-fire': '//cdn.firebase.com/libs/angularfire/0.3.0/angularfire',
     'firebase': '//cdn.firebase.com/v0/firebase',
@@ -19,8 +21,10 @@ require({
     'angular': {
       exports: 'angular'
     },
+    'angular-animate': ['angular'],
     'angular-cookies': ['angular'],
     'angular-resource': ['angular'],
+    'angular-route': ['angular'],
     'angular-sanitize': ['angular'],
     'angular-fire': ['angular', 'firebase', 'firebase-simple-login'],
     'firebase': {
